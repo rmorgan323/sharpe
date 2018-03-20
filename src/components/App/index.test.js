e9e97cfbe27af00a507a539c61a8e2a60f953c3c
Rob Morgan <rmorgan323@gmail.com>
2018-03-19 19:42:21 -0600

    Add getSparkline tests

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from './index';
 import { shallow } from 'enzyme';
+
+describe('App tests', () => {
+  // it('should match the snapshot', () => {
+  //   const renderedApp = shallow(<App />, {disableLifeCycleMethods: true});
+
+  //   expect(renderedApp).toMatchSnapshot();
+  // });
+});
 
 
